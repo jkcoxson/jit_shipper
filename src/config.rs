3107e5f3401998decfc40bb426cc7e04f9e17747
jkcoxson <jkcoxson@gmail.com>
2021-10-24 23:07:32 -0600

    Add pulling iOS dictionary

--- src/config.rs
+++ src/config.rs
@@ -4,65 +4,119 @@
 use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
+    env,
     fs::File,
     io::{Read, Write},
+    process::Output,
 };
 
 #[derive(Serialize, Deserialize)]
-pub struct Config {
-    devices: Vec<Device>,
-    versions: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize)]
 pub struct Device {
-    name: String,
-    version: String,
+    detected: bool,
+    pub name: String,
+    pub version: String,
     apps: HashMap<String, String>,
+    pub udid: String,
+    pub network: bool,
 }
 
-impl Config {
-    fn new() -> Config {
-        Config {
-            devices: Vec::new(),
-            versions: Vec::new(),
+impl Device {
+    pub fn new(udid: String, name: String, version: String, network: bool) -> Device {
+        Device {
+            detected: true,
+            name,
+            version,
+            apps: HashMap::new(),
+            udid,
+            network,
         }
     }
-    pub fn load(json_path: &str) -> Config {
-        match File::open(json_path) {
-            Ok(mut file) => {
-                let mut contents = String::new();
-                match file.read_to_string(&mut contents) {
-                    Ok(_) => match serde_json::from_str(&contents) {
-                        Ok(config) => config,
-                        Err(e) => {
-                            println!("Error parsing config file: {}", e);
-                            Config::new()
-                        }
-                    },
-                    Err(e) => {
-                        println!("Error reading config file: {}", e);
-                        Config::new()
+
+    pub fn device_scan() -> Vec<Device> {
+        match env::consts::OS {
+            "macos" => {
+                let output = std::process::Command::new("idevice_id")
+                    .output()
+                    .expect("Failed to execute process");
+                let output_str = String::from_utf8_lossy(&output.stdout);
+                let mut devices: Vec<Device> = Vec::new();
+                let first_line = match output_str.lines().nth(0) {
+                    Some(x) => x,
+                    None => return devices,
+                };
+                for line in first_line.split("\n") {
+                    if line.len() < 1 {
+                        continue;
                     }
+                    let line = output_str.split(" ");
+                    let line = line.collect::<Vec<&str>>();
+                    let udid = line[0].trim();
+                    let network = match line[1].trim() {
+                        "(Network)" => true,
+                        _ => false,
+                    };
+                    let mut found = false;
+                    for device in devices.iter_mut() {
+                        if device.udid == udid {
+                            found = true;
+                        }
+                        if found && network {
+                            device.network = true;
+                        }
+                    }
+                    if found {
+                        continue;
+                    }
+
+                    let info: Output;
+                    // Get device info
+                    if network {
+                        info = std::process::Command::new("ideviceinfo")
+                            .arg("-u")
+                            .arg("-n")
+                            .arg(udid)
+                            .output()
+                            .expect("Failed to execute process");
+                    } else {
+                        info = std::process::Command::new("ideviceinfo")
+                            .arg("-u")
+                            .arg(udid)
+                            .output()
+                            .expect("Failed to execute process");
+                    }
+
+                    let info = String::from_utf8_lossy(&info.stdout);
+                    let mut name = "";
+                    let mut verson = "";
+                    for line in info.split("\n") {
+                        if line.len() < 1 {
+                            continue;
+                        }
+                        let line = line.split(":");
+                        let line = line.collect::<Vec<&str>>();
+                        if line[0] == "DeviceName" {
+                            name = line[1].trim();
+                        }
+                        if line[0] == "ProductVersion" {
+                            verson = line[1].trim();
+                        }
+                    }
+
+                    let device = Device::new(
+                        udid.to_string(),
+                        name.to_string(),
+                        verson.to_string(),
+                        network,
+                    );
+                    devices.push(device);
                 }
+                devices
             }
-            Err(_) => {
-                println!("Error opening config file: {}", json_path);
-                Config::new()
+            // "linux" => {}
+            // "windows" => {}
+            _ => {
+                panic!("Unsupported OS");
             }
-        }
-    }
-    pub fn save(&self) {
-        let json_path = "config.json";
-        match File::create(json_path) {
-            Ok(mut file) => {
-                let json = serde_json::to_string_pretty(&self).unwrap();
-                match file.write_all(json.as_bytes()) {
-                    Ok(_) => println!("Saved config file: {}", json_path),
-                    Err(e) => println!("Error saving config file: {}", e),
-                }
-            }
-            Err(e) => println!("Error opening config file: {}", e),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // jkcoxson
 // All in one tool for activating JIT on iOS devices
 
-use std::env;
+use std::{convert::TryInto, env, fmt::format, fs::File, io, ptr::NonNull};
+
+use config::Device;
 mod config;
 mod install;
 mod user_input;
@@ -32,31 +34,91 @@ fn main() {
 }
 
 fn ui_loop() {
-    // Get home directory
+    // Get directory
     let home_dir = dirs::home_dir().unwrap();
-    // Detect if home_dir/libimobiledevice is present
     let libimobiledevice_path = home_dir.join("libimobiledevice");
-    let config_path = &libimobiledevice_path.join("config.json");
-    if !config_path.exists() {
-        std::fs::write(config_path.clone(), "{}").unwrap();
-    }
-    let config = config::Config::load(&config_path.to_str().unwrap());
+    // Change path to libimobiledevice
+    env::set_current_dir(&libimobiledevice_path).unwrap();
+
     loop {
-        env::set_current_dir(&libimobiledevice_path).unwrap();
-        match user_input::multi_input(
-            "What would you like to do?",
-            &["Launch an application", "Add an app", "Quit"],
-        )
-        .as_str()
-        {
-            "Launch an application" => {}
-            "Add an app" => {}
-            "Quit" => {
-                break;
+        match choose_device() {
+            Some(device) => {
+                let dmg_path = get_ios_dmg(&device);
             }
-            _ => {
-                panic!("Unhandled option");
+            None => {
+                println!("No devices detected, connect a device and then press enter");
+                std::io::stdin().read_line(&mut String::new()).unwrap();
             }
         }
     }
+}
+
+fn choose_device() -> Option<Device> {
+    let devices = Device::device_scan();
+    let mut options = vec![];
+    for device in &devices {
+        options.push(device.name.clone());
+    }
+    // Check if there are any devices
+    if options.len() == 0 {
+        return None;
+    }
+    // Convert strings to str array
+    let options: Vec<&str> = options.iter().map(|x| x.as_str()).collect();
+    // Convert options to an array
+    let options = options.as_slice();
+    let device_name = user_input::multi_input("Choose a device", options);
+
+    for device in devices {
+        if device.name == device_name {
+            return Some(device);
+        }
+    }
+
+    panic!("You shouldn't see this error message, this is just to make the compiler happy.");
+}
+
+fn get_ios_dmg(device: &Device) -> String {
+    // Get directory
+    let home_dir = dirs::home_dir().unwrap();
+    let libimobiledevice_path = home_dir.join("libimobiledevice");
+    let ios_path = &libimobiledevice_path.join(device.version.clone());
+    // Check if directory exists
+    if ios_path.exists() {
+        // Check if DMG exists
+        let ios_dmg = ios_path.join("DeveloperDiskImage.dmg");
+        if ios_dmg.exists() {
+            return ios_dmg.to_str().unwrap().to_string();
+        } else {
+            // Remove iOS directory
+            std::fs::remove_dir_all(ios_path).unwrap();
+        }
+    }
+    // Download versions.json from GitHub
+    println!("Downloading iOS dictionary...");
+    let url = "https://raw.githubusercontent.com/jkcoxson/jit_shipper/master/versions.json";
+    let response = reqwest::blocking::get(url).expect("Failed to download iOS version library");
+    let contents = response.text().expect("Failed to read iOS version library");
+    // Parse versions.json
+    let versions: serde_json::Value = serde_json::from_str(&contents).unwrap();
+    // Get DMG url
+    let ios_dmg_url = match versions.get(device.version.as_str()) {
+        Some(x) => x.as_str().unwrap().to_string(),
+        None => panic!(
+            "\nCould not find {} from the library. Check back later!\n",
+            device.version
+        ),
+    };
+    // Download DMG zip
+    let mut resp = reqwest::blocking::get(ios_dmg_url).expect("Unable to download DMG");
+    let mut out = File::create("dmg.zip").expect("Failed to create zip");
+    io::copy(&mut resp, &mut out).expect("failed to copy content");
+    // Unzip zip
+    let mut dmg_zip = zip::ZipArchive::new(File::open("dmg.zip").unwrap()).unwrap();
+    dmg_zip.extract(ios_path).unwrap();
+    // Remove zip
+    std::fs::remove_file("dmg.zip").unwrap();
+    // Return DMG path
+    let ios_dmg = ios_path.join("DeveloperDiskImage.dmg");
+    ios_dmg.to_str().unwrap().to_string()
 }
