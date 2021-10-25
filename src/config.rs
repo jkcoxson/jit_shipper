9aab614969c6ac545bd627a9b9daba9cd854b0e1
jkcoxson <jkcoxson@gmail.com>
2021-10-25 00:34:50 -0600

    Start adding other OS

--- src/config.rs
+++ src/config.rs
@@ -24,69 +24,70 @@ impl Device {
 
     pub fn device_scan() -> Vec<Device> {
         match env::consts::OS {
-            "macos" => {
-                let output = std::process::Command::new("idevice_id")
-                    .arg("-l")
-                    .output()
-                    .expect("Failed to execute process");
-                let output_str = String::from_utf8_lossy(&output.stdout);
-                let mut devices: Vec<Device> = Vec::new();
-                let first_line = match output_str.lines().nth(0) {
-                    Some(x) => x,
-                    None => return devices,
-                };
-                for line in first_line.split("\n") {
-                    if line.len() < 1 {
-                        continue;
-                    }
-                    let udid = output_str.trim();
-                    let mut found = false;
-                    for device in devices.iter_mut() {
-                        if device.udid == udid {
-                            found = true;
-                        }
-                    }
-                    if found {
-                        continue;
-                    }
-
-                    let info: Output;
-                    // Get device info
-                    info = std::process::Command::new("ideviceinfo")
-                        .arg("-u")
-                        .arg(udid)
-                        .output()
-                        .expect("Failed to execute process");
-
-                    let info = String::from_utf8_lossy(&info.stdout);
-                    let mut name = "";
-                    let mut verson = "";
-                    for line in info.split("\n") {
-                        if line.len() < 1 {
-                            continue;
-                        }
-                        let line = line.split(":");
-                        let line = line.collect::<Vec<&str>>();
-                        if line[0] == "DeviceName" {
-                            name = line[1].trim();
-                        }
-                        if line[0] == "ProductVersion" {
-                            verson = line[1].trim();
-                        }
-                    }
-
-                    let device =
-                        Device::new(udid.to_string(), name.to_string(), verson.to_string());
-                    devices.push(device);
-                }
-                devices
-            }
-            // "linux" => {}
-            // "windows" => {}
+            "macos" => Device::unix_scan(),
+            "linux" => Device::unix_scan(),
+            "windows" => todo!("Windows support coming soon TM"),
             _ => {
                 panic!("Unsupported OS");
             }
         }
+    }
+
+    pub fn unix_scan() -> Vec<Device> {
+        let output = std::process::Command::new("idevice_id")
+            .arg("-l")
+            .output()
+            .expect("Failed to execute process");
+        let output_str = String::from_utf8_lossy(&output.stdout);
+        let mut devices: Vec<Device> = Vec::new();
+        let first_line = match output_str.lines().nth(0) {
+            Some(x) => x,
+            None => return devices,
+        };
+        for line in first_line.split("\n") {
+            if line.len() < 1 {
+                continue;
+            }
+            let udid = output_str.trim();
+            let mut found = false;
+            for device in devices.iter_mut() {
+                if device.udid == udid {
+                    found = true;
+                }
+            }
+            if found {
+                continue;
+            }
+
+            let info: Output;
+            // Get device info
+            info = std::process::Command::new("ideviceinfo")
+                .arg("-u")
+                .arg(udid)
+                .output()
+                .expect("Failed to execute process");
+
+            let info = String::from_utf8_lossy(&info.stdout);
+            let mut name = "";
+            let mut verson = "";
+            for line in info.split("\n") {
+                if line.len() < 1 {
+                    continue;
+                }
+                let line = line.split(":");
+                let line = line.collect::<Vec<&str>>();
+                if line[0] == "DeviceName" {
+                    name = line[1].trim();
+                }
+                if line[0] == "ProductVersion" {
+                    verson = line[1].trim();
+                }
+            }
+
+            let device = Device::new(udid.to_string(), name.to_string(), verson.to_string());
+            devices.push(device);
+        }
+        devices
     }
 
     pub fn app_scan(&self) -> HashMap<String, String> {
@@ -123,6 +124,16 @@ impl Device {
                     .unwrap();
                 String::from_utf8_lossy(&output.stdout).to_string()
             }
+            "linux" => {
+                let output = std::process::Command::new("ideviceinstaller")
+                    .arg("-l")
+                    .arg("-u")
+                    .arg(self.udid.clone())
+                    .output()
+                    .unwrap();
+                String::from_utf8_lossy(&output.stdout).to_string()
+            }
+            "windows" => todo!("Windows support coming soon TM"),
             _ => {
                 panic!("Unsupported OS");
             }
@@ -147,6 +158,23 @@ impl Device {
                 }
                 true
             }
+            "linux" => {
+                let output = std::process::Command::new("idevicedebug")
+                    .arg("-u")
+                    .arg(self.udid.clone())
+                    .arg("--detach")
+                    .arg("run")
+                    .arg(pkg_identifier)
+                    .output()
+                    .expect("Failed to execute process");
+                let error = String::from_utf8_lossy(&output.stderr);
+                if error.len() > 0 {
+                    println!("{}", error);
+                    return false;
+                }
+                true
+            }
+            "windows" => todo!("Windows support coming soon TM"),
             _ => {
                 panic!("Unsupported OS");
             }
